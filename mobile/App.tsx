69376868ed768f7ac904096ff6c6cdc0d8d775dd
Daniel Zancheta <daniel.zancheta@amaggi.com.br>
2022-11-11 09:30:57 -0400

    finalizando aula 4 parte mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
@@ -20,7 +20,7 @@ export default function App() {
         translucent
 
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
 
   );
--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -7,7 +7,7 @@ export function Input(props: InputProps) {
         <input
             {...props}
             id="game"
-            placeholder="Selecione o game que deseja jogar"
+
             className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
         />
     )
